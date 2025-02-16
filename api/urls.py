fa475379714e9c74d92909fcccdcd588e30ac0d4
blessed <blesseddike70@gmail.com>
2025-02-16 20:46:44 +0100

    vee

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/room/",views.RoomView.as_view()),
     path("api/room/<int:pk>/",views.SingleRoomView.as_view()),
-    path("reservation/<int:pk>/",views.BookReservationView.as_view()),
+    path("api/reservation/<int:pk>/",views.BookReservationView.as_view()),
     path("api/check-reservation/",views.CheckReservationView.as_view()),
     path("api/cancel-reservation/<int:pk>/",views.CancelReservationView.as_view()),
     path("api/register/",views.RegisterView.as_view()),

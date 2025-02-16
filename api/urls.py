8d6223a1e83b2494920f8aae9a1201e9a1f107db
blessed <blesseddike70@gmail.com>
2025-02-16 11:44:26 +0100

    last ride

--- api/urls.py
+++ api/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/room/",views.RoomView.as_view()),
-    path("api/room/<int:pk>",views.SingleRoomView.as_view()),
+    path("api/room/<int:pk>/",views.SingleRoomView.as_view()),
     path("reservation/<int:pk>/",views.BookReservationView.as_view()),
     path("api/check-reservation/",views.CheckReservationView.as_view()),
-    path("api/cancel-reservation/<int:pk>",views.CancelReservationView.as_view()),
+    path("api/cancel-reservation/<int:pk>/",views.CancelReservationView.as_view()),
     path("api/register/",views.RegisterView.as_view()),
     path("",views.html_view,name="home")
 ]
